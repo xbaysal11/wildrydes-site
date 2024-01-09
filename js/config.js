28012f15225dc5d6a82115135eba64d8cdfee318
xbaysal11 <baisalbek.daniiarov@gmail.com>
2024-01-09 18:03:41 +0600

    ADD config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_PgxXWeCur', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '42sm2ntps0j563lr7516og1nvu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
